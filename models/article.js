721962368ddb19fa174302c3c039dcad8df9254c
VladLo939 <vlo194@yandex.ru>
2021-01-24 21:00:15 +0300

    add models

---
+++ models/article.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const schemaArticle = new mongoose.Schema({
+  keyword: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  source: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+    validator(v) {
+      return validator.isURL(v);
+    },
+    message: 'URL is invalid',
+  },
+  image: {
+    type: String,
+    require: true,
+    validator(v) {
+      return validator.isURL(v);
+    },
+    message: 'URL is invalid',
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    select: false,
+    ref: 'user',
+  },
+});
+
+module.exports = mongoose.model('article', schemaArticle);
---
+++ models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    index: true,
+    validate: {
+      validator(v) {
+        return validator.isEmail(v);
+      },
+      message: 'Email is invalid',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    select: false,
+  },
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+});
+
+module.exports = mongoose.model('user', userSchema);
